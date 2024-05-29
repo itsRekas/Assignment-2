d4061a1d86bf061ae13f9ab7a53769ea4be79a9d
Rekas <rka6631@nyu.edu>
2024-05-30 02:58:59 +0400

    I just figured the Java out
    hmm its 3:00am

---
+++ script.js
@@ -0,0 +1,43 @@
+let p1= document.getElementById("panel1");
+let p2= document.getElementById("panel2");
+let p3= document.getElementById("panel3");
+let p4= document.getElementById("panel4");
+let p5= document.getElementById("panel5");
+let p6= document.getElementById("panel6");
+let p7= document.getElementById("panel7");
+let p8= document.getElementById("panel8");
+let p9= document.getElementById("panel9");
+let p10= document.getElementById("panel10");
+let p11= document.getElementById("panel11");
+let p12= document.getElementById("panel12");
+
+let Panelarray=["panel1","panel2","panel3","panel4","panel5","panel6","panel7","panel8","panel9","panel10","panel11","panel12"]
+let Panelprev= ["panel1","panel1","panel2","panel3","panel3","panel2","panel6","panel7","panel4","panel5", "panel8", "panel8"]
+let Panelnexta=["panel2","panel3","panel5","panel9","panel10","panel7","panel8","panel11","panel9","panel10","panel11","panel12"]
+let Panelnextb=["panel2","panel6","panel4","panel9","panel10","panel7","panel8","panel12","panel9","panel10","panel11","panel12"]
+
+let currentindex=0;
+
+function getnext(x){
+    return x===Panelnexta[currentindex];
+}
+
+function getback(x){
+    return x===Panelprev[currentindex];
+}
+
+function nexta(){
+    document.getElementById(Panelarray[currentindex]).style.display='none';
+    currentindex=Panelarray.findIndex(getnext);
+    console.log(Panelarray.findIndex(getnext));
+    document.getElementById(Panelarray[currentindex]).style.display='block';
+}
+
+function back(){
+    document.getElementById(Panelarray[currentindex]).style.display='none';
+    currentindex=Panelarray.findIndex(getback);
+    console.log(Panelarray.findIndex(getback));
+    document.getElementById(Panelarray[currentindex]).style.display='block';
+}
+
+document.getElementById(Panelarray[currentindex]).style.display='block';
